ab52ecaafe1120af43c160f51737923d56e0f06a
Alexander <alexander.sabrera@sunqutech.com>
2021-10-29 07:03:01 -0500

    Limpieza de codigo

--- Domain/Useful/RandomGenerator.cs
+++ Domain/Useful/RandomGenerator.cs
@@ -10,9 +10,6 @@ namespace Domain.Useful
 
         }
 
-        // Instantiate random number generator.  
-        // It is better to keep a single Random instance 
-        // and keep using Next on the same instance.  
         private readonly Random _random = new Random();
 
         // Generates a random number within a range.      
@@ -25,13 +22,6 @@ namespace Domain.Useful
         public string RandomString(int size, bool lowerCase = false)
         {
             var builder = new StringBuilder(size);
-
-            // Unicode/ASCII Letters are divided into two blocks
-            // (Letters 65–90 / 97–122):   
-            // The first group containing the uppercase letters and
-            // the second group containing the lowercase.  
-
-            // char is a single Unicode character  
             char offset = lowerCase ? 'a' : 'A';
             const int lettersOffset = 26; // A...Z or a..z: length = 26  
 
