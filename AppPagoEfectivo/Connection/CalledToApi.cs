24cbf8405d63b8ea454c5cb166c2399649fa512c
Alexander <alexander.sabrera@sunqutech.com>
2021-10-28 20:08:50 -0500

    Correcciones cambio de api

---
+++ AppPagoEfectivo/Connection/CalledToApi.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AppPagoEfectivo.Connection
+{
+    public class CalledToApi
+    {
+        public Task<HttpResponseMessage> CallToApiGET(string urlBaseAddress, string requestUri, List<HeaderClass> listHeader)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(urlBaseAddress);
+
+                    foreach (var item in listHeader)
+                    {
+                        client.DefaultRequestHeaders.Add(item.HeaderName, item.HeaderValue);
+                    }
+
+                    HttpRequestMessage request = new HttpRequestMessage
+                    {
+                        Method = new HttpMethod("GET"),
+                        RequestUri = new Uri(client.BaseAddress + requestUri),
+                    };
+
+                    return client.SendAsync(request);
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+                httpResponseMessage.StatusCode = HttpStatusCode.BadRequest;
+                httpResponseMessage.Content = new StringContent(ex.Message);
+
+                return Task.FromResult(httpResponseMessage);
+            }
+        }
+
+        public async Task<HttpResponseMessage> CallToApiGET(string urlBaseAddress, string requestUri)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(urlBaseAddress);
+
+                    HttpRequestMessage request = new HttpRequestMessage
+                    {
+                        Method = new HttpMethod("GET"),
+                        RequestUri = new Uri(client.BaseAddress + requestUri),
+                    };
+
+                    return await client.SendAsync(request);
+                }
+            }
+            catch (Exception ex)
+            {
+                HttpResponseMessage httpResponseMessage = new HttpResponseMessage();
+                httpResponseMessage.StatusCode = HttpStatusCode.BadRequest;
+                httpResponseMessage.Content = new StringContent(ex.Message);
+
+                return Task.FromResult(httpResponseMessage).Result;
+            }
+        }
+
+    }
+}
---
+++ AppPagoEfectivo/Connection/HeaderClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppPagoEfectivo.Connection
+{
+    public class HeaderClass
+    {
+        public string HeaderName { get; set; }
+        public string HeaderValue { get; set; }
+    }
+}
